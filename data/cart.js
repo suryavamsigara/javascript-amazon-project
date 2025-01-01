b3cc042892e2349eba87807019ac45ad36b74d0d
SuryaVamsi <suryavamsigara2@gmail.com>
2025-01-01 19:30:27 +0530

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) { //product is already in the cart
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    })
+  }
+}
